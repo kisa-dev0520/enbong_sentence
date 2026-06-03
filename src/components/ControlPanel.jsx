299c7861d397efc95369018ac3b57517fee5488f
kisa-dev0520 <enbong0520@gmail.com>
2026-06-04 01:06:44 +0900

    vercel 배포_9

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -160,7 +160,8 @@ export default function ControlPanel({
   (잘못된 추천 : While she likes pizza, my firends like him. => 음식이 나와야 상식적)
 
 정답/오답 공통 : 관계사절에서 who/which를 생략한 문장일 경우 아래의 설명을 꼭 넣는다.
-"who/which 절에 주어가 있는 경우 생략하는 게 가장 자연스러워요. 만약 꼭 쓰고 싶다면 whom을 쓰는 게 자연스러워요. (실제 예시 문장 제시)
+"who/which 절에 주어가 있는 경우 생략하는 게 가장 자연스러워요. 만약 꼭 쓰고 싶다면 whom을 쓰는 게 자연스러워요.
+주의: 관계절은 제한적 용법으로 피드백 한다.
   
     
 JSON만 응답:
